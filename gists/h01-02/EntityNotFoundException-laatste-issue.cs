27a5eba2b707e339f1c9d7f16ae628beb739f364
sigged <sigged@users.noreply.github.com>
2020-09-20 00:48:09 +0200

    Create EntityNotFoundException-laatste-issue.cs

---
+++ gists/h01-02/EntityNotFoundException-laatste-issue.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Howest.Prog.CoinChop.Core.Common
+{
+    public class EntityNotFoundException : Exception
+    {
+        public EntityNotFoundException() : base()
+        {
+        }
+
+        public EntityNotFoundException(string message) : base(message)
+        {
+        }
+
+        public EntityNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
